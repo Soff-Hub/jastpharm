04510ed1b483292cd587c9593285829bbd031f91
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-06-12 17:49:58 +0500

    f

--- src/components/home/BrandLogos.tsx
+++ src/components/home/BrandLogos.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
 import api from "@/utils/api";
+import useResponsive from "@/hooks/use-responsive";
 
 interface PartnerItemType {
   id: number,
@@ -20,12 +21,14 @@ const BrandLogos = () => {
 
   const { t } = useTranslation()
   const [data, setData] = useState<PartnerItemType[]>([])
+  const { isMobile } = useResponsive()
 
   const getData = async () => {
     const resp = await api.get(`common/partners/`)
 
     setData(resp.data);
   }
+
 
   useEffect(() => {
     getData()
@@ -43,65 +46,62 @@ const BrandLogos = () => {
             </div>
           </div>
           <div className="mlr--55">
-            <div className="row brand-active d-flex align-items-center ">
-              <Swiper
+            <div className="row pt-25 pb-25">
+              {data.length && <Swiper
                 modules={[Scrollbar, A11y, Autoplay]}
-                spaceBetween={0}
+                spaceBetween={20}
                 loop={true}
                 observeParents={true}
                 observer={true}
                 centeredSlides={true}
-                slidesPerView={1}
+                slidesPerView={3}
                 autoplay={{
                   delay: 4000,
                   disableOnInteraction: true,
                 }}
-              // breakpoints={{
-              //   0: {
-              //     slidesPerView: 1,
-              //   },
-              //   500: {
-              //     slidesPerView: 3,
-              //   },
-              //   768: {
-              //     slidesPerView: 4,
-              //   },
-              //   992: {
-              //     slidesPerView: 4,
-              //   },
-              //   1200: {
-              //     slidesPerView: 4,
-              //   },
-              //   1400: {
-              //     slidesPerView: 6,
-              //   },
-              // }}
+                breakpoints={{
+                  0: {
+                    slidesPerView: 1,
+                  },
+                  365: {
+                    slidesPerView: 2,
+                  },
+                  576: {
+                    slidesPerView: 2,
+                  },
+                  768: {
+                    slidesPerView: 3,
+                  },
+                  992: {
+                    slidesPerView: 4,
+                  },
+                  1200: {
+                    slidesPerView: 4,
+                  },
+                  1400: {
+                    slidesPerView: 5,
+                  },
+                }}
               >
-                {data?.map((item, i) => (
-                  <SwiperSlide key={i}>
-                    <div className="row">
-                      {
-                        data?.slice(0, 6)?.map((el, j) => (
-                          <div key={j} className={`text-center brand-img-div col-md-4 p-0 ${(i + 1) % 3 === 0 ? 'border-start' : 'border-end'} ${(j >= 0 && j < 3) ? 'border-bottom' : ''}`}>
-                            <div className="p-5">
-                              <a href={el.url} target="_blank" className="d-block">
-                                <Image
-                                  className="d-inline-block brand-img"
-                                  src={el.image}
-                                  alt="brand-img"
-                                  width={160}
-                                  height={120}
-                                  style={{ width: '100%', height: 'auto', maxHeight: '80px', objectFit: 'contain' }}
-                                />
-                              </a>
-                            </div>
-                          </div>
-                        ))
-                      }
+                {[...data, ...data]?.map((el, j) => (
+                  <SwiperSlide key={j} className="brand-img-div col-md-4 d-flex align-items-center justify-content-center">
+                    <div key={j} className={`text-center  p-0`}>
+                      <div className="p-3">
+                        <a href={el.url} target="_blank" className="d-block">
+                          <Image
+                            className="d-inline-block brand-img"
+                            src={el.image}
+                            alt="brand-img"
+                            width={300}
+                            height={200}
+                            style={{ width: '100%', height: 'auto', objectFit: 'cover', maxHeight: '80px' }}
+                          />
+                        </a>
+                      </div>
                     </div>
                   </SwiperSlide>
                 ))}
-              </Swiper>
+              </Swiper>}
             </div>
           </div>
         </div>
